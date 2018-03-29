3a927a9f067975a3089a3babdf8434d59b15f370
JachyHm <jachym.hurtik@gmail.com>
2018-03-29 16:03:15 +0200

    Repaired Git automatization files

---
+++ GITnahraj.bat
@@ -0,0 +1,7 @@
+
+git add Assets/Smejki/CD460pack01/ Source/Smejki/CD460pack01/ GITnahraj.bat GITstahni.bat
+git commit
+git push -u origin master
+@echo off
+echo Ukoncete stisknutim libovolne klavesy...
+pause >nul
---
+++ GITstahni.bat
@@ -0,0 +1,4 @@
+git pull -u origin master
+@echo off
+echo Ukoncete stisknutim libovolne klavesy...
+pause >nul
